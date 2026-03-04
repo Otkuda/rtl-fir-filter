6709a2ac2073089ab045b06de8e8365cecdfa22a
Otkuda <kirkot.0306@gmail.com>
2026-03-04 22:52:48 +0300

    Complex signal generation and quantization

--- model/Signal.py
+++ model/Signal.py
@@ -17,6 +17,19 @@ def generateSinWave(freq: float, ph0: float, amp: float, fs: int, nSamples=2048,
   return amp * np.sin(PI2 * freq * ts + ph0)
 
 
+def generateComplexSinWave(freq: float, ph0: float, amp: float, fs:int, nSamples=2048, duration=None):
+
+  if nSamples == 0 and duration is not None: 
+    nSamples = round(fs * duration)
+    ts = np.linspace(0.0, duration, nSamples)
+  elif duration is None and nSamples != 0:
+    ts = np.arange(0, 1/fs * nSamples, 1/fs)
+  else:
+    return None
+
+  return amp * np.exp(1j * PI2 * freq * ts + ph0)
+
+
 def generateSquareWave(freq: float, ph0: float, amp: float, fs: int, nSamples=2048, duration=None):
   """
   Генерирует меандр
@@ -31,6 +44,12 @@ def generateSquareWave(freq: float, ph0: float, amp: float, fs: int, nSamples=20
 
   return amp * np.sign(np.sin(PI2 * freq * ts + ph0))
 
+def quantizeSignal(signal, maxValue: int, signed=True):
+  if signed:
+    res = np.clip(np.round(signal, 0), -maxValue, maxValue)
+  else:
+    res = np.clip(np.round(signal, 0), 0, maxValue)
+  return res
 
 def generateAMWave():
   pass
