acbb651bbb38e72cef91917c063c427befab5dc1
Otkuda <kirkot.0306@gmail.com>
2026-04-13 17:55:28 +0300

    Moved clipping to DSP

--- rtl/opt_complex_mac.sv
+++ rtl/opt_complex_mac.sv
@@ -31,28 +31,9 @@ generate
       .i_signal(i_signal_complex[DATA_WIDTH*(2-i)-1:DATA_WIDTH*(1-i)]),
       .i_coef(i_coef_real),
       .i_mac_clr(i_mac_clr),
-      .o_res(res[i])
+      .o_res(o_res_complex[DATA_WIDTH*(2-i)-1:DATA_WIDTH*(1-i)])
     );
     
-    logic pos_overflow;
-    logic neg_overflow;
-
-    assign pos_overflow = (res[i][MAC_WIDTH-1] == 1'b0) &&  (|res[i][MAC_WIDTH-2 : DATA_WIDTH-1]);
-    assign neg_overflow = (res[i][MAC_WIDTH-1] == 1'b1) && (~&res[i][MAC_WIDTH-2 : DATA_WIDTH-1]);
-
-    always_ff @(posedge clk) begin
-      if (rst) begin
-        o_res_complex[DATA_WIDTH*(2-i)-1:DATA_WIDTH*(1-i)] <= '0;
-      end
-      else begin
-        if (pos_overflow)
-          o_res_complex[DATA_WIDTH*(2-i)-1:DATA_WIDTH*(1-i)] <= MAX_VAL;
-        else if (neg_overflow)
-          o_res_complex[DATA_WIDTH*(2-i)-1:DATA_WIDTH*(1-i)] <= MIN_VAL;
-        else
-          o_res_complex[DATA_WIDTH*(2-i)-1:DATA_WIDTH*(1-i)] <= res[i][DATA_WIDTH-1:0];
-      end
-    end
   end
 
 endgenerate
--- rtl/opt_compute_mac.sv
+++ rtl/opt_compute_mac.sv
@@ -14,7 +14,7 @@ module opt_compute_mac #(
   input  logic signed [DATA_WIDTH-1:0] i_signal,
   input  logic signed [DATA_WIDTH-1:0] i_coef,
 
-  output logic signed [47:0] o_res,
+  output logic signed [DATA_WIDTH-1:0] o_res,
   input  logic i_mac_clr
 
 );
@@ -22,17 +22,27 @@ module opt_compute_mac #(
 localparam MULT_WIDTH  = DATA_WIDTH * 2;
 localparam FRAC_WIDTH  = DATA_WIDTH - 1; // without sign bit
 localparam ACC_WIDTH   = 48;  // accumulator as in DSP48E1
+localparam ROUND_WIDTH = ACC_WIDTH - FRAC_WIDTH;
 localparam logic signed [ACC_WIDTH-1:0] ROUND_CONST = (1'b1 << 14);
+
+localparam logic signed [DATA_WIDTH-1:0] MAX_VAL = (DATA_WIDTH)'(2**(DATA_WIDTH-1)-1);
+localparam logic signed [DATA_WIDTH-1:0] MIN_VAL = -(DATA_WIDTH)'(2**(DATA_WIDTH-1));
 
 logic mac_clr_0, mac_clr_1, mac_clr_2;
 
 logic signed [DATA_WIDTH-1:0] i_signal_reg_0, i_signal_reg_1;
 logic signed [DATA_WIDTH-1:0] i_coef_reg_0, i_coef_reg_1;
   
-logic signed [MULT_WIDTH-1:0] mul_res;
-logic signed [ ACC_WIDTH-1:0] acc_res;
+logic signed [MULT_WIDTH-1:0]  mul_res;
+logic signed [ ACC_WIDTH-1:0]  acc_res;
+logic signed [ROUND_WIDTH-1:0] round_res, round_res_1;
 
-logic signed [ ACC_WIDTH-1:0] c; 
+
+logic pos_overflow;
+logic neg_overflow;
+
+assign pos_overflow = (round_res[ROUND_WIDTH-1] == 1'b0) &&  (|round_res[ROUND_WIDTH-2 : DATA_WIDTH-1]);
+assign neg_overflow = (round_res[ROUND_WIDTH-1] == 1'b1) && (~&round_res[ROUND_WIDTH-2 : DATA_WIDTH-1]);
 
 
 always_ff @(posedge clk) begin
@@ -46,7 +56,10 @@ always_ff @(posedge clk) begin
     i_coef_reg_0 <= '0;
     i_signal_reg_1 <= '0;
     i_coef_reg_1 <= '0;
-    o_res <= '0;
+    round_res <= '0;
+    round_res_1 <= '0;
+    pos_overflow <= '0;
+    neg_overflow <= '0;
   end
   else begin
     // -- Stage 0 - Input copy -- 
@@ -70,11 +83,23 @@ always_ff @(posedge clk) begin
       acc_res <= acc_res + mul_res;
     end
 
-    // -- Stage 3 - output --
+    // -- Stage 3 - rounding --
     if (acc_res[FRAC_WIDTH-1:0] == {FRAC_WIDTH{1'b0}})
-      o_res <= {acc_res[ACC_WIDTH-1:FRAC_WIDTH+1], 1'b0}; 
+      round_res <= {acc_res[ACC_WIDTH-1:FRAC_WIDTH+1], 1'b0}; 
     else
-      o_res <= acc_res[ACC_WIDTH-1:FRAC_WIDTH];
+      round_res <= acc_res[ACC_WIDTH-1:FRAC_WIDTH];
+
+    pos_overflow <= (round_res[ROUND_WIDTH-1] == 1'b0) &&  (|round_res[ROUND_WIDTH-2 : DATA_WIDTH-1]);
+    neg_overflow <= (round_res[ROUND_WIDTH-1] == 1'b1) && (~&round_res[ROUND_WIDTH-2 : DATA_WIDTH-1]);
+    round_res_1 <= round_res;
+
+    // -- Stage 4 - clipping --
+    if (pos_overflow)
+      o_res <= MAX_VAL;
+    else if (neg_overflow)
+      o_res <= MIN_VAL;
+    else
+      o_res <= round_res_1[DATA_WIDTH-1:0];
   end
 end
 
